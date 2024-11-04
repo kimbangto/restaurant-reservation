567b281041ce82734c18f5824fc11f0bf30eeaa0
kimbangto <kimchaeyeon.525@gmail.com>
2024-11-04 16:50:44 +0900

    TypeAlias 수정

--- src/main/kotlin/com/sesac/restaurant/common/TypeAlias.kt
+++ src/main/kotlin/com/sesac/restaurant/common/TypeAlias.kt
@@ -1,11 +1,12 @@
 package com.sesac.restaurant.common
 
 import com.sesac.restaurant.model.*
+import java.time.LocalDate
 
 typealias GuestMap = MutableMap<String, Guest>
 typealias MenuMap = MutableMap<String, Menu>
 typealias OrderMap = MutableMap<Int, Order> // 테이블의 주문
 typealias TableOrderMap = MutableMap<Menu, Int> // 주문내용
 typealias ReservationMap = MutableMap<Int, Reservation>
-typealias TableMap = MutableMap<Int, Table>
+typealias TableMap = MutableMap<LocalDate, Map<Int, Table>> // 날짜별 테이블
 typealias BlackListMap = MutableMap<String, Guest>
