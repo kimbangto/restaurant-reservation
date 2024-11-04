d4aca544075cbaa1a902584bbc50ec393f680c28
kimbangto <kimchaeyeon.525@gmail.com>
2024-11-04 18:18:40 +0900

    Table Repository 메서드 추가 / Table의 reservation val -> var 변경

--- src/main/kotlin/com/sesac/restaurant/model/Table.kt
+++ src/main/kotlin/com/sesac/restaurant/model/Table.kt
@@ -1,4 +1,4 @@
 package com.sesac.restaurant.model
 
-class Table(val tableNumber: Int, val numberOfSeats: Int, val reservation: Reservation? = null, var isPaid: Boolean = false) {
+class Table(val tableNumber: Int, val numberOfSeats: Int, var reservation: Reservation? = null, var isPaid: Boolean = false) {
 }
