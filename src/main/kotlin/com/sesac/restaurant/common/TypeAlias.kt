6d0aee9e8f330ca95bd2669a20365c7308c2068d
kimbangto <kimchaeyeon.525@gmail.com>
2024-11-05 15:01:19 +0900

    Table 선택 출력 수정

--- src/main/kotlin/com/sesac/restaurant/common/TypeAlias.kt
+++ src/main/kotlin/com/sesac/restaurant/common/TypeAlias.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 
 typealias GuestMap = MutableMap<String, Guest>
 typealias MenuMap = MutableMap<String, Menu>
-typealias TableOrderMap = MutableMap<Menu, Int> // 주문내용
+typealias TableOrderMap = MutableMap<Menu, Int> // 주문내용(메뉴, 갯수)
 typealias ReservationMap = MutableMap<Int, Reservation>
 typealias TableMap = MutableMap<LocalDate, Map<Int, Table>> // 날짜별 테이블
 typealias BlackListMap = MutableMap<String, Guest>
